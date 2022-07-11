18b49df52d59398fd61deef58e57f33bbd3dabb6
Thainá Monteiro <96400582+Thaina-Monteiro@users.noreply.github.com>
2022-07-10 21:54:52 -0300

    Atualização

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 
 
-let [input1, input2]= document.querySelectorAll('input')
+let [input1, input2] = document.querySelectorAll('input')
 let acesso = document.querySelector('button')
-input1.addEventListener('input',hadleChange)
-input2.addEventListener('input',hadleChange)
+input1.addEventListener('input', hadleChange)
+input2.addEventListener('input', hadleChange)
 
-function hadleChange(){
-    if(input1.value && input2.value.length >= 8 ){
+function hadleChange() {
+    if (input1.value && input2.value.length >= 8) {
         acesso.classList.add('vermelho')
-    } else{
+    } else {
         acesso.classList.remove('vermelho')
     }
 }
